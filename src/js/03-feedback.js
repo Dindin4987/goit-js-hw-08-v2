507be42727c1a3f961e9e6d12175bbd64f6caf33
Dindin4987 <144334821+Dindin4987@users.noreply.github.com>
2024-02-26 18:53:40 +0800

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('form.feedback-form');
+const emailEl = document.querySelector('label [name="email"]');
+const messageEl = document.querySelector('label [name="messge"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+    const email = emailEl.value;
+    const message = messageEl.value;
+
+    const formData = {
+        email,
+        message,
+    };
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
