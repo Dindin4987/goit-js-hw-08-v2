98dc9987212604b8704fb72615c3b82293fe5e53
Dindin4987 <144334821+Dindin4987@users.noreply.github.com>
2024-02-26 11:39:46 +0800

    update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,27 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+galleryItems.forEach((item) => {
+    console.log(item);
+    let li = `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>
+`;
+    gallery.innerHTML += li;
+});
+const galleryHandler = new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
+galleryHandler.on("show.simplelightbox");
